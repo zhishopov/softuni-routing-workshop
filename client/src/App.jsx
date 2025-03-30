43b880f2273b91576a47a71a09b6bfd71b13d1e0
Zhivko Shopov <zhishopov@gmail.com>
2025-03-30 03:20:07 +0100

    Add game create route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import Header from "./components/header/Header";
 import Home from "./components/home/Home";
 import Login from "./components/login/Login";
 import Register from "./components/register/Register";
+import GameCreate from "./components/game-create/GameCreate";
 
 function App() {
   return (
@@ -16,6 +17,10 @@ function App() {
           <Route index element={<Home></Home>}></Route>
           <Route path="/login" element={<Login></Login>}></Route>
           <Route path="/register" element={<Register></Register>}></Route>
+          <Route
+            path="/games/create"
+            element={<GameCreate></GameCreate>}
+          ></Route>
         </Routes>
       </main>
     </div>
