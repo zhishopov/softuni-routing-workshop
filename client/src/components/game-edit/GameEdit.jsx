02bc51b040cc0163fdfc60c7345a1b1a89634cb8
Zhivko Shopov <zhishopov@gmail.com>
2025-03-30 16:35:41 +0100

    Show edit page with prepopulated values in the form

--- client/src/components/game-edit/GameEdit.jsx
+++ client/src/components/game-edit/GameEdit.jsx
@@ -1,4 +1,16 @@
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router";
+import gameService from "../../services/gameService";
+
 export default function GameEdit() {
+  const [game, setGame] = useState({});
+  const navigate = useNavigate();
+  const { gameId } = useParams();
+
+  useEffect(() => {
+    gameService.getOne(gameId).then(setGame);
+  }, [gameId]);
+
   return (
     <>
       {/* Edit Page ( Only for the creator )*/}
@@ -7,21 +19,36 @@ export default function GameEdit() {
           <div className="container">
             <h1>Edit Game</h1>
             <label htmlFor="leg-title">Legendary title:</label>
-            <input type="text" id="title" name="title" defaultValue="" />
+            <input
+              type="text"
+              id="title"
+              name="title"
+              defaultValue={game.title}
+            />
             <label htmlFor="category">Category:</label>
-            <input type="text" id="category" name="category" defaultValue="" />
+            <input
+              type="text"
+              id="category"
+              name="category"
+              defaultValue={game.category}
+            />
             <label htmlFor="levels">MaxLevel:</label>
             <input
               type="number"
               id="maxLevel"
               name="maxLevel"
               min={1}
-              defaultValue=""
+              defaultValue={game.maxLevel}
             />
             <label htmlFor="game-img">Image:</label>
-            <input type="text" id="imageUrl" name="imageUrl" defaultValue="" />
+            <input
+              type="text"
+              id="imageUrl"
+              name="imageUrl"
+              defaultValue={game.imageUrl}
+            />
             <label htmlFor="summary">Summary:</label>
-            <textarea name="summary" id="summary" defaultValue={""} />
+            <textarea name="summary" id="summary" defaultValue={game.summary} />
             <input
               className="btn submit"
               type="submit"
