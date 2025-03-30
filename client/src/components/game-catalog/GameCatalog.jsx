493b1680ac38633a8c545ebcd69402d5cf530187
Zhivko Shopov <zhishopov@gmail.com>
2025-03-30 04:21:08 +0100

    Render all games

--- client/src/components/game-catalog/GameCatalog.jsx
+++ client/src/components/game-catalog/GameCatalog.jsx
@@ -1,46 +1,34 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import gameService from "../../services/gameService";
 
 export default function GameCatalog() {
+  const [games, setGames] = useState([]);
+
   useEffect(() => {
-    gameService.getAll().then((result) => console.log(result));
-  });
+    gameService.getAll().then((result) => setGames(result));
+  }, []);
+
+  console.log(games);
+
   return (
     <>
       {/* Catalogue */}
       <section id="catalog-page">
         <h1>All Games</h1>
         {/* Display div: with information about every game (if any) */}
-        <div className="allGames">
-          <div className="allGames-info">
-            <img src="./images/avatar-1.jpg" />
-            <h6>Action</h6>
-            <h2>Cover Fire</h2>
-            <a href="#" className="details-button">
-              Details
-            </a>
+        {games.map((game) => (
+          <div key={game.id} className="allGames">
+            <div className="allGames-info">
+              <img src={game.imageUrl} />
+              <h6>{game.title}</h6>
+              <h2>{game.category}</h2>
+              <a href="#" className="details-button">
+                Details
+              </a>
+            </div>
           </div>
-        </div>
-        <div className="allGames">
-          <div className="allGames-info">
-            <img src="./images/avatar-1.jpg" />
-            <h6>Action</h6>
-            <h2>Zombie lang</h2>
-            <a href="#" className="details-button">
-              Details
-            </a>
-          </div>
-        </div>
-        <div className="allGames">
-          <div className="allGames-info">
-            <img src="./images/avatar-1.jpg" />
-            <h6>Action</h6>
-            <h2>MineCraft</h2>
-            <a href="#" className="details-button">
-              Details
-            </a>
-          </div>
-        </div>
+        ))}
+
         {/* Display paragraph: If there is no games  */}
         <h3 className="no-articles">No articles yet</h3>
       </section>
--- client/src/services/gameService.js
+++ client/src/services/gameService.js
@@ -3,8 +3,13 @@ import request from "../utils/request";
 const baseUrl = "http://localhost:3030/jsonstore/games";
 
 export default {
-  getAll() {
-    return request.get(baseUrl);
+  async getAll() {
+    const result = await request.get(baseUrl);
+
+    // Transform data to array
+    const games = Object.values(result);
+
+    return games;
   },
   create(gameData) {
     return request.post(baseUrl, gameData);
